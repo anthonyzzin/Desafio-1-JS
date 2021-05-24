811d9d166fe1e374636e904f66b2dd6c7a0f812f
anthonyzzin <80277056+anthonyzzin@users.noreply.github.com>
2021-05-24 16:38:13 -0300

    Desafio 1 em JS
    Vai na Web

---
+++ script.js
@@ -0,0 +1,12 @@
+let nome = "Anthony Nathan"
+let idade = 18
+let ano = 2021
+let nascimento = ano - idade
+console.log("Meu ano de nascimento é" ,nascimento)
+
+let amHuman = true
+
+const number = 10
+const numero = 11
+let soma = number + numero
+console.log("O resultado da soma entre os dois números é" ,soma)
